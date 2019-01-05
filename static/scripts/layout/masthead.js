b8eb85e1574d5c0460f2fb3c108be9b7db6fe0a0
ksuderman <suderman@cs.vassar.edu>
2019-01-05 09:51:01 -1000

    Why

--- static/scripts/layout/masthead.js
+++ static/scripts/layout/masthead.js
@@ -1,2 +1,132 @@
-define(["utils/utils","layout/menu","layout/scratchbook","mvc/user/user-quotameter"],function(a,b,c,d){var e=Backbone.View.extend({initialize:function(a){var e=this;this.options=a,this.setElement(this._template()),this.$navbarBrandLink=this.$(".navbar-brand-link"),this.$navbarBrandImage=this.$(".navbar-brand-image"),this.$navbarBrandTitle=this.$(".navbar-brand-title"),this.$navbarTabs=this.$(".navbar-tabs"),this.$quoteMeter=this.$(".quota-meter-container"),this.collection=new b.Collection,this.collection.on("add",function(a){e.$navbarTabs.append(new b.Tab({model:a}).render().$el)}).on("reset",function(){e.$navbarTabs.empty()}).on("dispatch",function(a){e.collection.each(function(b){a(b)})}).fetch(this.options),Galaxy.frame=this.frame=new c({collection:this.collection}),Galaxy.quotaMeter=this.quotaMeter=new d.UserQuotaMeter({model:Galaxy.user,el:this.$quoteMeter}),$(window).on("click",function(a){var b=$(a.target).closest("a[download]");1==b.length&&(0===$("iframe[id=download]").length&&$("body").append($("<iframe/>").attr("id","download").hide()),$("iframe[id=download]").attr("src",b.attr("href")),a.preventDefault())}).on("beforeunload",function(){var a="";return e.collection.each(function(b){var c=b.get("onbeforeunload")&&b.get("onbeforeunload")();c&&(a+=c+" ")}),""!==a?a:void 0})},render:function(){return this.$navbarBrandTitle.html((this.options.brand&&" "+this.options.brand+"/"||" ")+"Galaxy"),this.$navbarBrandLink.attr("href",this.options.logo_url),this.$navbarBrandImage.attr("src",this.options.logo_src),this.quotaMeter.render(),this},_template:function(){return'<div id="masthead" class="navbar navbar-fixed-top navbar-inverse"><div class="navbar-header"><div class="navbar-tabs"/></div><div class="navbar-brand"><a class="navbar-brand-link"><img class="navbar-brand-image"/><span class="navbar-brand-title"/></a></div><div class="quota-meter-container"/><div class="navbar-icons"/></div>'}});return{View:e}});
+define("layout/masthead", ["exports", "backbone", "layout/menu", "layout/scratchbook", "mvc/user/user-quotameter"], function(exports, _backbone, _menu, _scratchbook, _userQuotameter) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    var Backbone = _interopRequireWildcard(_backbone);
+
+    var _menu2 = _interopRequireDefault(_menu);
+
+    var _scratchbook2 = _interopRequireDefault(_scratchbook);
+
+    var _userQuotameter2 = _interopRequireDefault(_userQuotameter);
+
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
+
+    function _interopRequireWildcard(obj) {
+        if (obj && obj.__esModule) {
+            return obj;
+        } else {
+            var newObj = {};
+
+            if (obj != null) {
+                for (var key in obj) {
+                    if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
+                }
+            }
+
+            newObj.default = obj;
+            return newObj;
+        }
+    }
+
+    /* global Galaxy */
+    /* global $ */
+
+    /** Masthead **/
+    var View = Backbone.View.extend({
+        initialize: function initialize(options) {
+            var self = this;
+            this.options = options;
+            this.setElement(this._template());
+            this.$navbarBrandLink = this.$(".navbar-brand");
+            this.$navbarBrandImage = this.$(".navbar-brand-image");
+            this.$navbarBrandTitle = this.$(".navbar-brand-title");
+            this.$navbarTabs = this.$(".navbar-nav");
+            this.$quoteMeter = this.$(".quota-meter-container");
+
+            // build tabs
+            this.collection = new _menu2.default.Collection();
+            this.collection.on("add", function(model) {
+                self.$navbarTabs.append(new _menu2.default.Tab({
+                    model: model
+                }).render().$el);
+            }).on("reset", function() {
+                self.$navbarTabs.empty();
+            }).on("dispatch", function(callback) {
+                self.collection.each(function(m) {
+                    callback(m);
+                });
+            }).fetch(this.options);
+
+            // highlight initial active view
+            this.highlight(options.active_view);
+
+            // scratchbook
+            Galaxy.frame = this.frame = new _scratchbook2.default({
+                collection: this.collection
+            });
+
+            // set up the quota meter (And fetch the current user data from trans)
+            // add quota meter to masthead
+            Galaxy.quotaMeter = this.quotaMeter = new _userQuotameter2.default.UserQuotaMeter({
+                model: Galaxy.user,
+                el: this.$quoteMeter
+            });
+
+            // loop through beforeunload functions if the user attempts to unload the page
+            $(window).on("click", function(e) {
+                var $download_link = $(e.target).closest("a[download]");
+                if ($download_link.length == 1) {
+                    if ($("iframe[id=download]").length === 0) {
+                        $("body").append($("<iframe/>").attr("id", "download").hide());
+                    }
+                    $("iframe[id=download]").attr("src", $download_link.attr("href"));
+                    e.preventDefault();
+                }
+            }).on("beforeunload", function() {
+                var text = "";
+                self.collection.each(function(model) {
+                    var q = model.get("onbeforeunload") && model.get("onbeforeunload")();
+                    if (q) {
+                        text += q + " ";
+                    }
+                });
+                if (text !== "") {
+                    return text;
+                }
+            });
+        },
+
+        render: function render() {
+            // this.$navbarBrandTitle.html(`Galaxy ${(this.options.brand && `/ ${this.options.brand}`) || ""}`);
+            this.$navbarBrandTitle.html((this.options.brand && this.options.brand + " /" || " ") + " Galaxy");
+            this.$navbarBrandLink.attr("href", this.options.logo_url);
+            this.$navbarBrandImage.attr("src", this.options.logo_src);
+            this.quotaMeter.render();
+            return this;
+        },
+
+        highlight: function highlight(id) {
+            this.collection.forEach(function(model) {
+                model.set("active", model.id == id);
+            });
+        },
+
+        /** body template */
+        _template: function _template() {
+            return "\n            <nav id=\"masthead\" class=\"navbar navbar-expand fixed-top justify-content-center navbar-dark\">\n                <a class=\"navbar-brand\">\n                    <img class=\"navbar-brand-image\"/>\n                    <span class=\"navbar-brand-title\"/>\n                </a>\n                <ul class=\"navbar-nav\"/>\n                <div class=\"quota-meter-container\"/>\n            </nav>";
+        }
+    });
+
+    exports.default = {
+        View: View
+    };
+});
 //# sourceMappingURL=../../maps/layout/masthead.js.map
